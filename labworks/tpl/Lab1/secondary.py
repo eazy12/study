250f9074a58124bd153540efcc0b377cd1b06fe5
eazy12 <eazy12kzn@gmail.com>
2016-10-04 00:07:04 +0300

    tpl: v2

--- labworks/tpl/Lab1/secondary.py
+++ labworks/tpl/Lab1/secondary.py
@@ -24,30 +24,6 @@ class LA:
         self.label1.grid(row=0, column=1,sticky="n")
         self.root.rowconfigure(0, weight=1)
         self.root.columnconfigure(0, weight=1)
-    def random_msg(self):
-        msg1 = "000" * random.randint(0,5) + "010" + "011" * random.randint(0,5)
-        g = random.randint(1,10)
-        k = random.randint(1,10)
-        msg2=""
-        while(g != 0):
-            e = random.randint(1,4)
-            if e ==1:
-                msg2+="a"
-            elif e==2:
-                msg2+="b"
-            elif e==3:
-                msg2+="c"
-            elif e==4:
-                msg2+="d"
-            g = g -1
-        q = msg2.find("bb")
-        while(q != -1 ):
-            msg2= msg2[:q] + msg2[(q+1):]
-            q = msg2.find("bb")
-        if(k>6):
-            return msg1 + msg2 + "#" + msg2[e:k]
-        else:
-            return msg1 + msg2
     def lex_anal(self):
         if(self.la_condition == self.condition.finish):
             self.text1.delete(1.0, END)
@@ -59,27 +35,23 @@ class LA:
             self.bufferString = self.text1.get(1.0, END)
 
         self.b = ''
-        for self.i in enumerate(self.bufferString[:-1]):
-            self.slovo = ""
-            self.I = iter(self.bufferString)
-            if self.i[1] == ' ':
-                self.type = 'Space'
-                continue
-            elif '0' <= self.i[1] <= '1':
-                self.type = 'Number'
-                if self.i[1] =='0':
-                    self.A_digit()
-                elif self.i[1] =='1':
-                    print("error #1")
-            elif 'a' <= self.i[1] <= 'z':
-                self.type = "Letter"
-            elif self.i[1] == '#':
-                self.type = "Comm"
-            self.z = self.b + str(self.i[1]) + '-' + str(ord(self.i[1]))+ '-'+ self.type + ' '
-            if (len(self.z) <= self.endtryWidth ):
-                self.b+= str(self.i[1]) + '-' + str(ord(self.i[1]))+ '-' + self.type + ' '
+        self.k = 0
+
+        for i in self.bufferString[:-1]:
+            if i == ' ':
+                self.type = 'space'
+            elif '0' <= i <= '9':
+                self.type = 'digit'
+            elif 'a' <= i <= 'z':
+                self.type = 'Letter'
+            elif i == '#':
+                self.type = "comm-ry"
+            self.z = self.b + str(i) + ' ' + str(ord(i))+ ' '+ self.type
+            if (len(self.z) <= 31 ):
+                self.k +=1
+                self.b+= str(i) + ' ' + str(ord(i))+ ' ' + self.type
             else:
-                self.bufferString = self.bufferString[self.i[0]:]
+                self.bufferString = self.bufferString[self.k:]
                 self.la_condition = self.condition.contin
                 self.label1['text']="Continue"
                 break
@@ -89,72 +61,3 @@ class LA:
             self.bufferString = ''
         self.b = self.b[:-1]+ ' '
         self.entry1.insert(1 ,self.b)
-
-    def A_digit(self):
-        if(self.i[1]=='0'):
-            self.slovo += self.i[1]
-            next(self.I)
-            print(self.slovo + '+' + self.i[1])
-            self.B_digit()
-        elif self.i[1] =='1':
-            print("Error 2")
-    def B_digit(self):
-        print("@")
-        if(self.i[1]=='0'):
-            self.slovo += self.i[1]
-            yield self.i[1]
-            print(self.slovo + ' ' + self.i[1])
-            self.C_digit()
-        elif(self.i[1]=='1'):
-            self.slovo += self.i[1]
-            yield self.i[1]
-            print(self.slovo + ' ' + self.i[1])
-            self.D_digit()
-        else:
-            print("Error")
-    def C_digit(self):
-        if(self.i[1]=='0'):
-            self.slovo += self.i[1]
-            yield self.i[1]
-            print(self.slovo + ' ' + self.i[1])
-            self.A_digit()
-        else:
-            print("Error")
-    def D_digit(self):
-        if(self.bufferString[i[0]+1]=="0"):
-            self.slovo += self.i[1]
-            yield self.i[1]
-            print(self.slovo + ' ' + self.i[1])
-            self.E_digit()
-        elif self.bufferString[i[0]+1]=="1":
-            print("Error")
-        else:
-            self.FIN_digit()
-    def E_digit(self):
-        if(self.i[1]=='0'):
-            self.slovo += self.i[1]
-            yield self.i[1]
-            print(self.slovo + ' ' + self.i[1])
-            self.F_digit()
-        elif self.i[1]=='1':
-            print("error")
-    def F_digit(self):
-        if self.i[1] == '1':
-            self.slovo += self.i[1]
-            yield self.i[1]
-            print(self.slovo + ' ' + self.i[1])
-            self.G_digit()
-        elif self.i[1]=='0':
-            print("error")
-    def G_digit(self):
-        if(self.bufferString[i[0]+1]=="0"):
-            self.slovo += self.i[1]
-            yield self.i[1]
-            print(self.slovo + ' ' + self.i[1])
-            self.E_digit()
-        elif self.bufferString[i[0]+1]=="1":
-            print("Error")
-        else:
-            self.FIN_digit()
-    def FIN_digit(self):
-        self.entry1.insert(1.0, self.slovo + " Верно")
