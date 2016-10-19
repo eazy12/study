11122dc3d5b5d594c4239a1624291af6aa648370
eazy12 <eazy12@eazy12debian>
2016-10-19 09:32:47 +0300

    tpl: lab2 is done; rc2

--- labworks/tpl/Lab1/v2/secondary.py
+++ labworks/tpl/Lab1/v2/secondary.py
@@ -1,25 +1,19 @@
 from tkinter import *
 from enum import Enum
+
 class LA:
 	def __init__(self):
-		self.textWidth = 25
-		self.entryWidth = 35
+		self.textWidth = 20
+		self.entryWidth = 45
 		self.root=Tk()
 		self.strr = ""
-		self.isEntryFull = False
 		self.condition = "start"
 		self.where = ""
-		self.oldWhere = ""
 		self.endStatus = False
-		self.endSymbol=""
 		self.obligatory = False
 		self.entryString = ""
-		self.errorStatus = ""
-		self.b = False
-		self.slovo = ""
 		self.i = 0
 		self.type1 = ""
-		self.isItCommentary = False
 		self.text1=Text(self.root,height=20,width=self.textWidth,font='Mono 14',wrap=WORD)
 		self.entry1=Entry(self.root,width=self.entryWidth,font='Mono 14')
 		self.button1=Button(self.root,text="Start",command=self.lex_anal )
@@ -29,7 +23,6 @@ class LA:
 		self.root.rowconfigure(0, weight=1)
 		self.root.columnconfigure(0, weight=1)
 	def lex_anal(self):
-		# if( self.strr == ""):
 		self.entry1.delete(0, END)
 		com = False
 		for j, value in enumerate(self.text1.get(1.0, END)):
@@ -63,6 +56,7 @@ class LA:
 						continue
 					else:
 						print("Error in begin")
+						self.entryString += self.strr[self.i]
 						self.skip("error")
 				else:
 					if self.where == "A_digit()":
@@ -79,8 +73,6 @@ class LA:
 						self.F_digit()
 					elif self.where =="G_digit()":
 						self.G_digit()
-					elif self.where =="FIN_digit()":
-						self.FIN_digit()
 					elif self.where =="A_letter()":
 						self.A_letter()
 					elif self.where =="B_letter()":
@@ -90,7 +82,6 @@ class LA:
 				break
 
 	def skip(self, instr):
-		print("Johny?")
 		if(instr == "error"):
 			self.entry1.insert(len(self.entry1.get()) , self.entryString)
 			for j in self.strr[self.i:]:
@@ -285,7 +276,7 @@ class LA:
 			self.type1 = " буквы "
 		elif self.strr[self.i] == ' ':
 			self.i+=1
-			self.entry1.insert(len(self.entry1.get()) , self.entryString + ' ' + "буквы")
+			self.entry1.insert(len(self.entry1.get()) , self.entryString + ' ' + " буквы ")
 			self.condition="start"
 			self.entryString =""
 			return
@@ -315,7 +306,7 @@ class LA:
 		if (self.strr[self.i] in "abcd"):
 			self.type1 = " буквы "
 		elif self.strr[self.i] == ' ':
-			self.entry1.insert(len(self.entry1.get()) , self.entryString + ' ' + "буквы")
+			self.entry1.insert(len(self.entry1.get()) , self.entryString + ' ' + " буквы ")
 			self.i+=1
 			self.condition="start"
 			self.entryString =""
