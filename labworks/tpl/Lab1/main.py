dd57fd96a9b332e062d8bf0766349ad49633cbf4
eazy12 <eazy12kzn@gmail.com>
2016-11-11 01:13:31 +0300

    labworks: files was rearranged

--- labworks/tpl/Lab1/main.py
+++
@@ -1,6 +0,0 @@
-# todo2: возвращать код только определенных символов
-
-
-from secondary import LA
-la = LA()
-la.root.mainloop()
---
+++ labworks/tpl/lab4/main.py
@@ -0,0 +1,6 @@
+from secondary import LA
+from secondary import UI
+la = LA()
+ui = UI(la)
+la.set_ui(ui)
+ui.root.mainloop()
